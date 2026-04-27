d4a5a75947a514342ba48543e0739f8b4b38a902
Jacob Tartabini <139934788+jacobtartabini@users.noreply.github.com>
2026-04-27 12:30:21 -0400

    Update capacitor.config.ts

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.jacobtartabini.arlo',
-  appName: 'arlo-ai',
+  appId: 'com.tartabinienterprises.arlo',
+  appName: 'arlo',
   webDir: 'dist',
   
   // Live reload configuration for development
